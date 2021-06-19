95d120ea4e301aaf9ad92a1ee3af5753fa528e6d
Kash <kserenitym@gmail.com>
2021-06-18 23:43:19 -0500

    addEmployee functional

--- scripts/script.js
+++ scripts/script.js
@@ -4,32 +4,23 @@ const employees= [];
 
 function onReady(){ 
     //add necessary functions to get all the moving parts working together
-addEmployee;
-
-
-
+    $( '#submitAll' ).on('click', addEmployee); 
 
 }
 
 
 
-function addEmployee(firstNameInput, lastNameInput, employeeIDInput, jobTitleInput, annualSalaryInput){
-    //function to take input values and append them to table
-    console.log('In addEmployee', addEmployee);
-    const addEmployeeObject = {
-        firstName: firstNameInput,
-        lastName: lastNameInput,
-        employeeID: employeeIDInput,
-        jobTitle: jobTitleInput,
-        annualSalary: annualSalaryInput
+function addEmployee(){
+    //function to take input values and add to empty array
+    const employeeObject = {
+        firstName: $('#firstNameInput').val(),
+        lastName: $('#lastNameInput').val(),
+        employeeID: $('#iDInput').val(),
+        jobTitle: $('#jobTitleInput').val(),
+        annualSalary: $('#annualSalaryInput').val()
     }
-    employees.push(addEmployeeObject);
-    $( '#submitAll' ).on('click', function(){ 
-        //button needs to push inputs into array and then array needs to be displayed on table
-        employees($('#firstNameInput').val(), $('#lastNameInput').val(), $('#iDInput').val(), $('#jobTitleInput').val(), $('#annualSalaryInput').val());
-
-    });
-
+    employees.push(employeeObject);
+    console.log(employees);
 }
 
 function attachToTable(){
