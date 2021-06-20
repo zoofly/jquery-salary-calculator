dc89637189667761a7aed9ced83a23e1310a2b35
Kash <kserenitym@gmail.com>
2021-06-20 16:02:34 -0500

    changed styling of total monthly

--- scripts/script.js
+++ scripts/script.js
@@ -7,13 +7,13 @@ function onReady(){
     $( '#submitAll' ).on('click', addEmployee);
     $( '#submitAll' ).on('click', attachToTable);
     $( '#submitAll' ).on('click', emptyInput);
-    
+    // $( '#deleteButton').on('click', deleteEmployee);
 
 
 }
 
 
-
+//function pushes employeeObjects into employees array
 function addEmployee(){
     //function to take input values and add to empty array
     const employeeObject = {
@@ -25,24 +25,31 @@ function addEmployee(){
     }
     employees.push(employeeObject);
     console.log(employees);
-    emptyInput();
+    emptyInput(); 
 }
 
-function attachToTable(){
+function attachToTable(){     
     for (let person of employees){
-        $('.arrayObjects').append(`<td> ${person.firstName}</td>`);
-        $('.arrayObjects').append(`<td> ${person.lastName}</td>`);
-        $('.arrayObjects').append(`<td> ${person.employeeID}</td>`);
-        $('.arrayObjects').append(`<td> ${person.jobTitle}</td>`);
-        $('.arrayObjects').append(`<td> ${person.annualSalary}</td>`);
+        $('.arrayObjects').closest('tr').append(`<td> ${person.firstName}</td>`);
+        $('.arrayObjects').closest('tr').append(`<td> ${person.lastName}</td>`);
+        $('.arrayObjects').closest('tr').append(`<td> ${person.employeeID}</td>`);
+        $('.arrayObjects').closest('tr').append(`<td> ${person.jobTitle}</td>`);
+        $('.arrayObjects').closest('tr').append(`<td> $${person.annualSalary}</td>`);
     }
-}
+    $('.arrayObjects').closest('tr').append(`<td> <input type="button" id="deleteButton" value="Delete"</td>`)
+ }
 
-function emptyInput(){
+function emptyInput(){ //empties input values
     $('#firstNameInput').val('');
     $('#lastNameInput').val('');
     $('#iDInput').val('');
     $('#jobTitleInput').val('');
     $('#annualSalaryInput').val('');
 }
+
+function deleteEmployee(){
+    employees.splice(0,1);
+}
+
+
 
